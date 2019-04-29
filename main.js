486edbe06ba3bd832b9b59d6dd48dd583f89adb2
Elizabeth Emig <epitman523@gmail.com>
2019-04-29 11:02:12 -0500

    connected main.js, added jQuery

---
+++ main.js
@@ -0,0 +1,7 @@
+//Goal: Only show single fieldset/button at a time Button toggles view on next pair
+
+//1. Show first question on doc ready
+    //target .js-group-1 and toggle class .current
+//2. When group-1 btn clicked --> show next pair
+    //on click toggle class on current and on next pair
+
